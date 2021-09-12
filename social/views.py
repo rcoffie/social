863c60c2fe65aab205ea3acc081b9f50c5cc2904
Robert <rcoffie22@yahoo.com>
2021-09-12 08:06:44 +0000

    post toggler

--- social/views.py
+++ social/views.py
@@ -5,8 +5,17 @@ from .forms import *
 # Create your views here.
 
 def Index(request):
-  form = PostForm()
+  
   posts = Post.objects.all()
+  form = PostForm()
+  if request.method == 'POST':
+    form = PostForm(request.POST)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.author = request.user
+      post.save()
+      return redirect('social:index')
+      
   context = {'posts':posts,'form':form}
   
   return render(request,'social/index.html',context)
