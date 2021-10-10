e82a4a72980d82019fd7594b3beb533440714f21
Robert <rcoffie22@yahoo.com>
2021-10-10 15:24:02 +0000

    working on profile 2

--- social/models.py
+++ social/models.py
@@ -27,6 +27,7 @@ class UserProfile(models.Model):
   name = models.CharField(max_length=30, blank=True, null=True)
   bio  = models.TextField(max_length=500, blank=True, null=True)
   birth_date = models.DateField(null=True, blank=True)
+  location   = models.CharField(max_length=50, blank=True, null=True)
   picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/images.jpg', blank=True)
   
   
