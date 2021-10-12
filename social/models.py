7945196c4883f90d64c9d4dde3c6e6338866d8fa
Robert <rcoffie22@yahoo.com>
2021-10-12 19:08:32 +0000

    auto creating user profile when user create and accounts

--- social/models.py
+++ social/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone 
 from datetime import datetime 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+from django.db.models.signals import post_save 
+from django.dispatch import receiver 
 
 # Create your models here.
 
@@ -29,6 +31,18 @@ class UserProfile(models.Model):
   birth_date = models.DateField(null=True, blank=True)
   location   = models.CharField(max_length=50, blank=True, null=True)
   picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/images.jpg', blank=True)
+  
+  
+  
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+  if created: 
+    UserProfile.objects.create(user=instance)
+    
+    
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+  instance.profile.save()
   
   
   
